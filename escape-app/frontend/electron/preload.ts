4b7cbb1d596a29c684e562443b9c47c4e14f7ad7
sieglou <111538904+sieglou@users.noreply.github.com>
2025-04-09 19:56:37 +0300

    Add escape-app/frontend/electron/preload.ts 1744217796792

---
+++ escape-app/frontend/electron/preload.ts
@@ -0,0 +1,5 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electron', {
+  invoke: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args)
+});
